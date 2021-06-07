9c45e39d3cebcf42c80bd7c91197a159131a8d06
Anomalous Alpha <blackcarrera@msn.com>
2021-06-06 18:57:50 -0600

    can create employee, add role as seen as role name and create employee, none of that id number crap

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const newRole = () => {
                 type: 'list',
                 name: 'title',
                 message: 'Enter name for a role?',
-                choices: ['Manager', 'Supervisor', 'Sales', 'Customer Service',]
+                choices: ['Manager', 'Subordinate',]
             },
             {
                 type: 'input',
@@ -128,11 +128,11 @@ const newEmployee = () => {
 }
 
 const createManager = () => {
-    const depatmentQuery = `SELECT roles.id AS role_id, roles.role_title, departments.id AS dept_id, departments.dept_title FROM roles INNER JOIN departments ON roles.id = departments.id`
+    const depatmentQuery = `SELECT * FROM departments`
     db.query(depatmentQuery, (err, res) => {
         const departmentChoices = res.map(({ role_id, role_title, dept_id, dept_title }) => ({
-            name: `${role_title, dept_title }`,
-            value: `${role_id, dept_id }`
+            name: `${role_title, dept_title}`,
+            value: `${role_id, dept_id}`
         }))
         inquirer.prompt([
             {
@@ -172,137 +172,137 @@ const createManager = () => {
 
 
 const createSubordinate = () => {
-        const roleQuery = `SELECT role_title FROM roles WHERE role_title != ('Manager')`
-        db.query(roleQuery, (err, res) => {
-            const roleChoices = res.map(({ id, role_title }) => ({
-                name: role_title,
+    const roleQuery = `SELECT * FROM roles WHERE roles.role_title  != ('Manager')`
+    db.query(roleQuery, (err, res) => {
+        const roleChoices = res.map(({ id, role_title }) => ({
+            name: role_title,
+            value: id
+        }))
+
+        const managerQuery = `SELECT * FROM employees WHERE role_id = 1`
+        db.query(managerQuery, (err, res) => {
+            const managerChoices = res.map(({ id, first_name, last_name }) => ({
+                name: `${first_name} ${last_name}`,
                 value: id
             }))
 
-            const managerQuery = `SELECT * FROM employees WHERE role_id = 1`
-            db.query(managerQuery, (err, res) => {
-                const managerChoices = res.map(({ id, first_name, last_name }) => ({
-                    name: `${first_name} ${last_name}`,
-                    value: id
-                }))
-
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'firstname',
-                        message: 'Enter employee first name',
-                    },
-                    {
-                        type: 'input',
-                        name: 'lastname',
-                        message: 'Enter employee last name',
-                    },
-                    {
-                        type: 'list',
-                        name: 'role',
-                        message: "Enter employee's role",
-                        choices: roleChoices
-                    },
-                    {
-                        type: 'list',
-                        name: 'manager',
-                        message: "Enter employee's manager",
-                        choices: managerChoices
-                    }
-                ])
-                    .then(data => {
-                        const query = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('${data.firstname}','${data.lastname}','${data.role}','${data.manager}')`
-                        db.query(query, (err, res) => {
-                            console.table(res);
-                            whatToDo();
-                        })
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'firstname',
+                    message: 'Enter employee first name',
+                },
+                {
+                    type: 'input',
+                    name: 'lastname',
+                    message: 'Enter employee last name',
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: "Enter employee's role",
+                    choices: roleChoices
+                },
+                {
+                    type: 'list',
+                    name: 'manager',
+                    message: "Enter employee's manager",
+                    choices: managerChoices
+                }
+            ])
+                .then(data => {
+                    const query = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('${data.firstname}','${data.lastname}','${data.role}','${data.manager}')`
+                    db.query(query, (err, res) => {
+                        console.table(res);
+                        whatToDo();
                     })
-            })
+                })
         })
-    }
+    })
+}
 
-    const updateEmployeeRole = () => {
-        const newRoleQuery = `SELECT * FROM roles`
-        db.query(newRoleQuery, (err, res) => {
-            const newRoleChoices = res.map(({ id, title }) => ({
-                name: title,
+const updateEmployeeRole = () => {
+    const newRoleQuery = `SELECT * FROM roles`
+    db.query(newRoleQuery, (err, res) => {
+        const newRoleChoices = res.map(({ id, title }) => ({
+            name: title,
+            value: id
+        }))
+        const selectEmployeeQuery = `SELECT * FROM employees WHERE manager_id IS NOT NULL`
+        db.query(selectEmployeeQuery, (err, res) => {
+            const employeeChoices = res.map(({ id, first_name, last_name }) => ({
+                name: `${first_name} ${last_name}`,
                 value: id
             }))
-            const selectEmployeeQuery = `SELECT * FROM employees WHERE manager_id IS NOT NULL`
-            db.query(selectEmployeeQuery, (err, res) => {
-                const employeeChoices = res.map(({ id, first_name, last_name }) => ({
-                    name: `${first_name} ${last_name}`,
-                    value: id
-                }))
-                inquirer.prompt([
-                    {
-                        type: 'list',
-                        name: 'newRole',
-                        message: 'Enter a new role id number',
-                        choices: newRoleChoices
-                    },
-                    {
-                        type: 'list',
-                        name: 'employee',
-                        message: 'Enter employee id number to update role',
-                        choices: employeeChoices
-                    },
-                ])
-                    .then(data => {
-                        const query = `UPDATE employees SET role_id = ${data.newRole} WHERE id = ${data.employee}`
-                        db.query(query, (err, res) => {
-                            console.table(res);
-                            whatToDo();
-                        })
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'newRole',
+                    message: 'Enter a new role id number',
+                    choices: newRoleChoices
+                },
+                {
+                    type: 'list',
+                    name: 'employee',
+                    message: 'Enter employee id number to update role',
+                    choices: employeeChoices
+                },
+            ])
+                .then(data => {
+                    const query = `UPDATE employees SET role_id = ${data.newRole} WHERE id = ${data.employee}`
+                    db.query(query, (err, res) => {
+                        console.table(res);
+                        whatToDo();
                     })
-            })
+                })
         })
-    }
+    })
+}
 
-    // Query functions to display tables
-    const viewAllDepartments = () => {
-        console.log(`
+// Query functions to display tables
+const viewAllDepartments = () => {
+    console.log(`
 ================================================
                 All Deparments!
 ================================================
     `
-        )
-        const query = "SELECT * FROM departments"
-        db.query(query, (err, res) => {
-            console.table(res);
-            whatToDo();
-        })
-    }
+    )
+    const query = "SELECT * FROM departments"
+    db.query(query, (err, res) => {
+        console.table(res);
+        whatToDo();
+    })
+}
 
-    const viewAllRoles = () => {
-        console.log(`
+const viewAllRoles = () => {
+    console.log(`
 ================================================
                   All Roles!
 ================================================
         `
-        )
-        const query = 'SELECT * FROM roles'
-        db.query(query, (err, res) => {
-            console.table(res);
-            whatToDo();
-        })
-    }
+    )
+    const query = 'SELECT * FROM roles'
+    db.query(query, (err, res) => {
+        console.table(res);
+        whatToDo();
+    })
+}
 
-    const viewAllEmployees = () => {
-        console.log(`
+const viewAllEmployees = () => {
+    console.log(`
 ================================================
  All Employees, Roles, Department and Managers!
 ================================================
         `
-        )
-        const query = 'SELECT * FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id'
-        db.query(query, (err, res) => {
-            console.table(res);
-            whatToDo();
-        })
-    }
+    )
+    const query = 'SELECT * FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id'
+    db.query(query, (err, res) => {
+        console.table(res);
+        whatToDo();
+    })
+}
 
-    whatToDo();
+whatToDo();
 
 
     // SELECT employees.id, employees.first_name, employees.last_name, roles.id AS role_id, roles.title AS role_name, departments.id, departments.title FROM employees INNER JOIN roles ON employees.id = roles.id INNER JOIN departments ON roles.id = departments.id;
